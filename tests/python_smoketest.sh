0703387c18fbc25015faf11d8aa36dfc7dbb89d3
alcantarar <ryan.alcantara@colorado.edu>
2020-11-27 14:13:20 -0700

    update python smoketest

--- tests/python_smoketest.sh
+++ tests/python_smoketest.sh
@@ -8,15 +8,15 @@
 
 
 echo 'Testing scripts' \
-	&& echo 'Show me a peach body! (script_1.py)' \
+	&& echo 'Give me a U! (script_1.py)' \
 	&& python_scripts/script_1.py \
-	&& echo 'Show me a leaf! (script_2.py)' \
+	&& echo 'Give me a W! (script_2.py)' \
 	&& python_scripts/script_2.py \
-	&& echo 'Show me a stem and halo! (script_3.py)' \
+	&& echo 'Give me an M! (script_3.py)' \
 	&& python_scripts/script_3.py \
-	&& echo 'Show me a the logo! (script_4.py)' \
+	&& echo 'Give me a banner! (script_4.py)' \
 	&& python_scripts/script_4.py \
-	&& echo 'Show me the text! (script_5.py)' \
+	&& echo 'Give me a banner of different color! (script_5.py)' \
 	&& python_scripts/script_5.py \
 
 echo 'All together now. Running main_script.py' \
