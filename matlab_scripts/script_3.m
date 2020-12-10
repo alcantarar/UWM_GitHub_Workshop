1c794c321ff135a71d3a61181758498f90589e70
Farhad Balali <farhad.balali@jci.com>
2020-12-10 17:18:03 -0600

    Commit Script 3

--- matlab_scripts/script_3.m
+++ matlab_scripts/script_3.m
@@ -3,16 +3,16 @@
 %   This script produces the third letter in the logo.
 %
 
-%read in data
-data = csvread('data/letter_3.csv');
-%plot data
-l = patch(data(:,1), data(:,2), 'black');
-l.LineWidth = 2;
-
-%% SOLUTION: Breakout 1
-
 % %read in data
 % data = csvread('data/letter_3.csv');
 % %plot data
-% l = patch(data(:,1), data(:,2), [1, 0.8, 0]);  % per reviewer comment
+% l = patch(data(:,1), data(:,2), 'black');
 % l.LineWidth = 2;
+
+%% SOLUTION: Breakout 1
+
+%read in data
+data = csvread('data/letter_3.csv');
+%plot data
+l = patch(data(:,1), data(:,2), [1, 0.8, 0]);  % per reviewer comment
+l.LineWidth = 2;
