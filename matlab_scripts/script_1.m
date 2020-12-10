421b5eae65d7e59cfeaf6a02c6fb266ded263d44
Josh Leonardis <josh.leonardis@gmail.com>
2020-12-10 18:24:04 -0500

    Updated script 1

--- matlab_scripts/script_1.m
+++ matlab_scripts/script_1.m
@@ -6,7 +6,7 @@
 %read in data
 data = csvread('data/letter_1.csv');
 %plot data
-p = patch(data(:,1), data(:,2), 'black');
+p = patch(data(:,1), data(:,2), [1 0.8 0]);
 p.LineWidth = 2;
 
 %% SOLUTION: Breakout 1
